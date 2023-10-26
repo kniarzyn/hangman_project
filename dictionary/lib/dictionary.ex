122bafc317825963033ed3776a7cac2e2f42bce3
kniarzyn <kniarzyn@gmail.com>
2023-10-26 14:19:59 +0200

    First version of Dictionary

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,18 +1,12 @@
 defmodule Dictionary do
-  @moduledoc """
-  Documentation for `Dictionary`.
-  """
+  def random_word do
+    word_list()
+    |> Enum.random()
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Dictionary.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defp word_list do
+    "assets/words.txt"
+    |> File.read!()
+    |> String.split("\n", trim: true)
   end
 end
